6f35d7463269ce455de0e97cfbafefc8df729058
Damandeep Singh <damandeepsingh@viewlift.com>
2022-10-10 12:20:05 +0530

    import-resolved

--- js/receiver.js
+++ js/receiver.js
@@ -2,7 +2,7 @@ const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 const requestData = new cast.framework.messages.SeekRequestData()
 const playbackConfig = new cast.framework.PlaybackConfig();
-const languageMap=require('./assets/languageMap.json');
+import * as languageMap from './assets/languageMap.json';
 
 
 // Listen and log all Core Events.
